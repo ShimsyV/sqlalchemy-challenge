57ce9ddd1ec28b6bbdc047c7afe0d4feef2e7550
Shimsy Varkey <shimsyanna@gmail.com>
2020-10-12 23:07:41 -0500

    Lists Date, low, avg and high temperature from the start date

--- climate_app.py
+++ climate_app.py
@@ -42,6 +42,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     """List all available api routes."""
+    print("Server received request for 'Home' page...")
     return (
         f"Avalable Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -58,46 +59,65 @@ def home():
 
 # Precipitation route
 
+# Precipitation Route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-        # Convert the query results to a dictionary using date as the key and prcp as the value    
-        print("Server received request for 'precipitation' page...")
+        print("Server received request for 'Precipitation' page...")
         # Calculate the Date 1 Year Ago from the Last Data Point in the Database
         one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
-        
+        # Design a Query to Retrieve the Last 12 Months of Precipitation Data Selecting Only the `date` and `prcp` Values
         prcp_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).order_by(Measurement.date).all()
-        
-        # Convert List of Tuples Into a Dictionary
+        # Convert List of Tuples into a Dictionary
         prcp_data_list = dict(prcp_data)
-
         # Return JSON Representation of Dictionary
         return jsonify(prcp_data_list)
 
 
 # Stations route
 
+# Station Route
 @app.route("/api/v1.0/stations")
 def stations():
-      # Return a JSON List of Stations From the Dataset
+        print("Server received request for 'Stations' page...")
+        # Return a JSON List of Stations from the Dataset
         stations_all = session.query(Station.station, Station.name).all()
-        # Convert List of Tuples Into Normal List
+        # Convert List of Tuples into a dictionary
         station_list = list(stations_all)
         # Return JSON List of Stations from the Dataset
         return jsonify(station_list)
 
-
 # TOBS route
-
 @app.route("/api/v1.0/tobs")
 def tobs():
-     # Query for the Dates and Temperature Observations from a Year from the Last Data Point
+        print("Server received request for 'tobs' page...")
+        # Query for the Dates and Temperature Observations from a Year from the Last Data Point
         one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
         # Design a Query to Retrieve the Last 12 Months of temperature Data Selecting Only the `date` and `tobs` Values
         tobs_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= one_year_ago).order_by(Measurement.date).all()
-        # Convert List of Tuples Into Normal List
+        # Convert List of Tuples into dictionary
         tobs_data_list = list(tobs_data)
         # Return JSON List of Temperature Observations (tobs) for the Previous Year
         return jsonify(tobs_data_list)
+
+# Start Day Route
+@app.route("/api/v1.0/<start>")
+def start_day(start):
+    print("Server received request for 'start' page...")
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    results =  (session.query(*sel).filter(func.strftime("%Y-%m-%d", Measurement.date) >= start).group_by(Measurement.date).all())
+
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
+
+
 
 
 # This final if statement simply allows us to run in "Development" mode, which 
